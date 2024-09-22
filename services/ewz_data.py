a44d911ac09494c3c83ed1d4054a0777dc0b7f71
Samuel <admin@sgart.dev>
2024-09-23 00:59:08 +0200

    Remove unused files

--- services/ewz_data.py
+++
@@ -1,9 +0,0 @@
-from services.general import fetch_html_content
-
-URL = "https://www.ewz.ch/de/private/strom/tarife/tarifuebersicht.html"
-
-async def fetch_low_tariff():
-    content = fetch_html_content(URL)
-
-
-
--- services/general.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def fetch_html_content(url):
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        return response.text
-    else:
-        return ""
